5de1f64c437382feeaaa7cad4eddf1768e8b0edf
HA KH <dgroyale111011@gmail.com>
2022-01-29 15:16:53 +0330

    Refactor names

--- API/main.go
+++ API/main.go
@@ -4,13 +4,13 @@ import (
 	"github.com/DevTeam125/shopping-website/config"
 	"github.com/DevTeam125/shopping-website/models"
 	"github.com/DevTeam125/shopping-website/models/product"
-	"github.com/DevTeam125/shopping-website/router"
+	"github.com/DevTeam125/shopping-website/routers"
 )
 
 func main() {
 	config.Init()
 	models.Init()
 	product.Init()
-	routes := router.InitRoutes()
+	routes := routers.InitRoutes()
 	routes.Run()
 }
