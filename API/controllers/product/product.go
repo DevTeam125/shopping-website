af28d6930c74e1321f06dfe7c24e090cbaf4cbd3
HA KH <dgroyale111011@gmail.com>
2022-01-31 15:15:41 +0330

    Complete CRUD operations for products

--- API/controllers/product/product.go
+++ API/controllers/product/product.go
@@ -3,16 +3,32 @@ package product
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/DevTeam125/shopping-website/models/product"
 	"github.com/gin-gonic/gin"
 )
 
+func GetProductByID(c *gin.Context) {
+	var product product.Product
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+	result, err := product.GetProductByID(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"ok": true, "result": result})
+}
+
 func GetAllProductsBrief(c *gin.Context) {
 	var productBrief product.Product
 	result, err := productBrief.GetAllProductsBrief(0, 100)
 	if err != nil {
-		log.Printf(err.Error())
+		log.Println(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"ok": false})
 		return
 	}
@@ -24,14 +40,19 @@ func AddNewProduct(c *gin.Context) {
 	err := c.BindJSON(&product1)
 
 	if err != nil {
-		log.Printf(err.Error())
+		log.Println(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
 		return
 	}
 
 	if product1.ID != nil {
-		log.Printf("product.ID is set")
+		log.Println("product.ID is set")
 		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+
+	if product1.Name == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false, "error": "Field name is mandatory"})
 		return
 	}
 
@@ -40,35 +61,87 @@ func AddNewProduct(c *gin.Context) {
 
 	err = product1.SaveProduct()
 	if err != nil {
-		log.Printf(err.Error())
+		log.Println(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"ok": false})
 		return
 	}
 
 	if features != nil {
-		for i, _ := range features {
+		for i := range features {
 			features[i].ProductID = *product1.ID
 		}
 
 		err = product.SaveFeatures(features)
 		if err != nil {
-			log.Printf(err.Error())
+			log.Println(err.Error())
 			c.JSON(http.StatusInternalServerError, gin.H{"ok": false})
 			return
 		}
 	}
 
 	if photos != nil {
-		for i, _ := range photos {
+		for i := range photos {
 			photos[i].ProductID = *product1.ID
 		}
 
 		err = product.SavePhotos(photos)
 		if err != nil {
-			log.Printf(err.Error())
+			log.Println(err.Error())
 			c.JSON(http.StatusInternalServerError, gin.H{"ok": false})
 			return
 		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"ok": true})
+}
+
+func UpdateProduct(c *gin.Context) {
+	var productToBeUpdated product.Product
+	err := c.BindJSON(&productToBeUpdated)
+
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+
+	if productToBeUpdated.ID == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false, "error": "Field ID is mandatory"})
+		return
+	}
+
+	done, err := productToBeUpdated.UpdateProduct()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+
+	if done {
+		c.JSON(http.StatusOK, gin.H{"ok": true})
+		return
+	}
+
+	c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+}
+
+func DeleteProductByID(c *gin.Context) {
+	var product product.Product
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+
+	done, err := product.DeleteProductByID(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
+	}
+
+	if !done {
+		c.JSON(http.StatusBadRequest, gin.H{"ok": false})
+		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"ok": true})
